29e6a171eea478df482d14c63693c4777a312f1d
waxhsu <98721786+waxhsu@users.noreply.github.com>
2025-01-09 15:04:24 -0500

    courseschedule spacing update

--- src/components/CourseSchedule/CourseSchedule.jsx
+++ src/components/CourseSchedule/CourseSchedule.jsx
@@ -10,6 +10,7 @@ export default function CourseSchedule() {
     return (
         <section >
             <section className='courseSchedule' >
+            <div className='courseSchedule-pretext'>
                 <h2>Outcome-Driven UX Metrics: <br />
                     Your Online Course Schedule</h2>
                 <h4>8 Hours live with Jared Spool in small group coaching, plus 8 hours of recorded content.</h4>
@@ -22,8 +23,8 @@ export default function CourseSchedule() {
                 </p>
                 <p>We know that schedules are tough, so we record every coaching session with Jared. If you miss a session, or you just want to review what you discussed, you’ll have access to the recordings for 6 months after the course ends. (You can also watch the recordings from other cohorts, to hear how they worked through their biggest challenges.)</p>
                 
+            </div>
                 <Calendar />
-
             </section>
         </section>
     )
